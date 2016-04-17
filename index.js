289aec07e89cb5b3ccac37125e40435c4fc06451
Maheshkumar <mahesh@raykor.com>
2016-04-17 10:31:21 +0530

    Fixed first char is not included in allow chars.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * Generate password form allowed word
+ * Generate password from allowed word
  */
 var digits = '0123456789';
 var alphabets = 'abcdefghijklmnopqrstuvwxyz';
@@ -7,29 +7,38 @@ var upperCase = alphabets.toUpperCase();
 var specialChars = '#!&@';
 var defaultOptions = { digits: true, alphabets: true, upperCase: true, specialChars: true };
 
-module.exports = {};
-
 function rand(min, max) {
 	var random = Math.random();
     return Math.floor(random * (max - min) + min);
 };
 
-module.exports.generate = function (length, options) {
-	var length = length || 10;
-	var generateOptions = Object.create(defaultOptions);
-    generateOptions.digits = options.digits;
-    generateOptions.alphabets = options.alphabets;
-    generateOptions.upperCase = options.upperCase;
-    generateOptions.specialChars = options.specialChars;
-	var allowsChars = ((generateOptions.digits || '') && digits) +
-		((generateOptions.alphabets || '') && alphabets) +
-		((generateOptions.upperCase || '') && upperCase) +
-		((generateOptions.specialChars || '') && specialChars);
-	var password = '';
-	for (var index = 0; index < length; ++index) {
-		var charIndex = rand(1, allowsChars.length);
-		password += allowsChars[charIndex];
+module.exports = {
+	/**
+	 * Generate OTP of the length
+	 * @param  {number} length length of password.
+	 * @param  {object} options
+	 * @param  {boolean} options.digits Default: `true` true value includes digits in OTP 
+	 * @param  {boolean} options.alphabets Default: `true` true value includes alphabets in OTP
+	 * @param  {boolean} options.upperCase Default: `true` true value includes upperCase in OTP
+	 * @param  {boolean} options.specialChars Default: `true` true value includes specialChars in OTP
+	 */
+	generate: function (length, options) {
+		var length = length || 10;
+		var generateOptions = Object.create(defaultOptions);
+		generateOptions.digits = options.digits;
+		generateOptions.alphabets = options.alphabets;
+		generateOptions.upperCase = options.upperCase;
+		generateOptions.specialChars = options.specialChars;
+		var allowsChars = ((generateOptions.digits || '') && digits) +
+			((generateOptions.alphabets || '') && alphabets) +
+			((generateOptions.upperCase || '') && upperCase) +
+			((generateOptions.specialChars || '') && specialChars);
+		var password = '';
+		for (var index = 0; index < length; ++index) {
+			var charIndex = rand(0, allowsChars.length - 1);
+			password += allowsChars[charIndex];
+		}
+		return password;
 	}
-	return password;
-};
 
+}
