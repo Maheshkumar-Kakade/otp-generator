da4102e9e8a58d0bcf2090a5deef9dcc2e29f36d
Maheshkumar Kakade <maheshkumarkakade@gmail.com>
2020-08-09 14:36:12 +0530

    Updated to latest dependancies and support for nodejs 10 and greater fix for #7

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /**
  * Generate password from allowed word
  */
-var digits = '0123456789'
-var alphabets = 'abcdefghijklmnopqrstuvwxyz'
-var upperCase = alphabets.toUpperCase()
-var specialChars = '#!&@'
+const digits = '0123456789'
+const alphabets = 'abcdefghijklmnopqrstuvwxyz'
+const upperCase = alphabets.toUpperCase()
+const specialChars = '#!&@'
 
 function rand (min, max) {
-  var random = Math.random()
+  const random = Math.random()
   return Math.floor(random * (max - min) + min)
 }
 
@@ -23,20 +23,20 @@ module.exports = {
    */
   generate: function (length, options) {
     length = length || 10
-    var generateOptions = options || {}
+    const generateOptions = options || {}
 
-    generateOptions.digits = generateOptions.hasOwnProperty('digits') ? options.digits : true
-    generateOptions.alphabets = generateOptions.hasOwnProperty('alphabets') ? options.alphabets : true
-    generateOptions.upperCase = generateOptions.hasOwnProperty('upperCase') ? options.upperCase : true
-    generateOptions.specialChars = generateOptions.hasOwnProperty('specialChars') ? options.specialChars : true
+    generateOptions.digits = Object.prototype.hasOwnProperty.call(generateOptions, 'digits') ? options.digits : true
+    generateOptions.alphabets = Object.prototype.hasOwnProperty.call(generateOptions, 'alphabets') ? options.alphabets : true
+    generateOptions.upperCase = Object.prototype.hasOwnProperty.call(generateOptions, 'upperCase') ? options.upperCase : true
+    generateOptions.specialChars = Object.prototype.hasOwnProperty.call(generateOptions, 'specialChars') ? options.specialChars : true
 
-    var allowsChars = ((generateOptions.digits || '') && digits) +
+    const allowsChars = ((generateOptions.digits || '') && digits) +
       ((generateOptions.alphabets || '') && alphabets) +
       ((generateOptions.upperCase || '') && upperCase) +
       ((generateOptions.specialChars || '') && specialChars)
-    var password = ''
-    for (var index = 0; index < length; ++index) {
-      var charIndex = rand(0, allowsChars.length - 1)
+    let password = ''
+    while (password.length < length) {
+      const charIndex = rand(0, allowsChars.length - 1)
       password += allowsChars[charIndex]
     }
     return password
